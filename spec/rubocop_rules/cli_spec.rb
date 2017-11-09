adbf1e869c4c35332ae7ff89ff13d3814a9e6af0
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-11-09 13:53:04 +0100

    Add cli specs

---
+++ spec/rubocop_rules/cli_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'rubocop_rules/cli'
+
+RSpec.describe RubocopRules::CLI do
+  describe '$ rubocop-rules' do
+    it 'prints help text' do
+      output = capture(:stdout) { RubocopRules::CLI::Commands.start([]) }
+      expect(output).to include 'help [COMMAND]  # Describe available commands or one specific command'
+    end
+  end
+
+  describe '$ rubocop-rules init' do
+    it 'copies rubocop_common', :silent do
+      expect_any_instance_of(RubocopRules::CLI::Commands)
+        .to receive(:copy_file)
+        .with('.rubocop_common.yml', '.rubocop_common.yml')
+
+      expect_any_instance_of(RubocopRules::CLI::Commands)
+        .to receive(:copy_file)
+        .with('.rubocop.yml', '.rubocop.yml')
+
+      expect_any_instance_of(RubocopRules::CLI::Commands)
+        .to receive(:copy_file)
+        .with('spec/lint_spec.rb', 'spec/lint_spec.rb')
+
+      RubocopRules::CLI::Commands.start(['init'])
+    end
+  end
+
+  describe '$ rubocop-rules update', :silent do
+    it 'copies rubocop_common' do
+      expect_any_instance_of(RubocopRules::CLI::Commands)
+        .to receive(:copy_file)
+        .with('.rubocop_common.yml', '.rubocop_common.yml')
+
+      RubocopRules::CLI::Commands.start(['update'])
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,21 @@
 require 'bundler/setup'
 require 'rubocop_rules'
 
+Dir.entries('./spec/support').select { |f| f =~ /\.rb$/ }.each do |f|
+  load "./spec/support/#{f}"
+end
+
 RSpec.configure do |config|
+  include CLIHelper
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.around(:each, :silent) do |example|
+    capture(:stdout) { example.run }
+  end
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
